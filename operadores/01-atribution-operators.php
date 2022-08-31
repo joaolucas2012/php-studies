ce2a18e5edced1f32be425c3ad35bb188180367b
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-08-31 16:04:44 -0300

    :book: start learning about atribution operators

---
+++ operadores/01-atribution-operators.php
@@ -0,0 +1,25 @@
+<?php
+
+  /* Operadores de atribuição */
+  $nome = "Jonas";
+
+  // operador de concatenação
+  echo $nome . " mais alguma coisa <br/>";
+  $nome.= " Treinamento";
+  echo $nome;
+
+  /************************/
+
+  $valor = 0;
+  echo $valor . " ";;
+
+  $valor += 15;
+  echo $valor . " ";;
+
+  $valor -= 5;
+  echo $valor . " ";;
+
+  $valor *= .3;
+  echo $valor;
+
+?>
