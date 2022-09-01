b542201e25cffa27008ee8d7d4dbe600a492f750
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-09-01 11:59:07 -0300

    :book: start learning about quotes to print strings

---
+++ strings/01-strings-quotes.php
@@ -0,0 +1,17 @@
+<?php 
+
+$nome = "Hcode";
+$nome2 = "Treinamento";
+
+var_dump($nome, $nome2);
+
+echo "<br/>";
+
+// aspas duplas printa o valor da variavel
+echo "Oi $nome $nome2";
+
+echo "<br/>";
+
+// aspas simples printa o texto somente
+echo 'Oi $nome $nome2';
+?>
