fdf41ab8c6582e2da3bdb7432a4167ceff3f0ae0
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-09-05 12:20:28 -0300

    :book: learning switch case

---
+++ 4-estruturas-de-controle-e-repetição/2-switch-case.php
@@ -0,0 +1,35 @@
+<?php
+
+// pega o dia da semana
+$diaDaSemana = date("w");
+
+// Teste data inválida
+// $diaDaSemana = 8;
+
+switch ($diaDaSemana) {
+  case 0:
+    echo "Domingo";
+    break;
+  case 1:
+    echo "Segunda";
+    break;
+  case 2:
+    echo "Terça";
+    break;
+  case 3:
+    echo "Quarta";
+    break;
+  case 4:
+    echo "Quinta";
+    break;
+  case 5:
+    echo "Sexta";
+    break;
+  case 6:
+    echo "Sábado";
+    break;
+  default:
+    echo "Data inválida";
+}
+
+?>
