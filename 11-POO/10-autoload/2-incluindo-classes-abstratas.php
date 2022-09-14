d99ab2b67f6474b9b81c38c5a5db760431c41793
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-09-14 09:31:59 -0300

    :rocket: learning more about autoload

--- 11-POO/10-autoload/2-incluindo-classes-abstratas.php
+++ 11-POO/10-autoload/2-incluindo-classes-abstratas.php
@@ -1,6 +1,17 @@
 <?php
 
-spl_autoload_register
+function incluirClasses($nomeClasse){
+  if(file_exists($nomeClasse.".php")){
+    require_once("$nomeClasse.php");
+  }
+}
+
+spl_autoload_register("incluirClasses");
+spl_autoload_register(function($nomeClasse){
+  if(file_exists("abstratas" . DIRECTORY_SEPARATOR . $nomeClasse.".php")){
+    require_once("abstratas" . DIRECTORY_SEPARATOR . "$nomeClasse.php");
+  }
+});
 
 $carro = new DelRey();
 $carro->acelerar(20);
