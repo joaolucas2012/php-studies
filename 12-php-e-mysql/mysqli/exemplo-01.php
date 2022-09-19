186591e029246fbf43caa250f4f32c116aac744d
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-09-19 15:43:28 -0300

    :fire: add more info about the database

--- 12-php-e-mysql/mysqli/exemplo-01.php
+++ 12-php-e-mysql/mysqli/exemplo-01.php
@@ -36,4 +36,32 @@ $password = "senha";
 
 $stmt->execute();
 
+// SCRIPT PARA CRIAÇÃO DO BANDO DE DADOS 
+
+/*
+USE dbphp7;
+
+CREATE TABLE tb_usuarios (
+idusuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+deslogin VARCHAR(64) NOT NULL,
+dessenha VARCHAR(256) NOT NULL,
+dtcadastro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()
+);
+
+INSERT INTO tb_usuarios (deslogin, dessenha) VALUES('root', '!@#$%');
+
+SELECT * FROM tb_usuarios;
+
+UPDATE tb_usuarios SET dessenha = '123456' WHERE idusuario = 1;
+
+SELECT * FROM tb_usuarios;
+
+DELETE FROM tb_usuarios WHERE  idusuario = 1;
+
+SELECT * FROM tb_usuarios;
+
+TRUNCATE TABLE tb_usuarios;
+
+*/
+
 ?>
