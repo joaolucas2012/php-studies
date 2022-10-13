00c98b49f63b5f0ba7cb63fb09c30bd6f8f1396a
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-10-13 10:28:45 -0300

    :book: keep learning about creating thumbnails with GD

--- 16-grafic-design-GD/4-gerando-thumbnail-GD.php
+++ 16-grafic-design-GD/4-gerando-thumbnail-GD.php
@@ -5,8 +5,8 @@ header("Content-Type: image/jpeg");
 $file = "images/wallpaper.jpg";
 
 // Redimensionando a imagem com altura e largura novas
-$new_width = 256;
-$new_height = 256;
+$new_width = 720;
+$new_height = 480;
 
 // Pegando o tamanho da imagem carregada
 /* 
@@ -22,18 +22,27 @@ $oldImage = imagecreatefromjpeg($file);
 
 /*
 imagecopyresampled(
-    GdImage $dst_image,
-    GdImage $src_image,
-    int $dst_x,
-    int $dst_y,
-    int $src_x,
-    int $src_y,
-    int $dst_width,
-    int $dst_height,
-    int $src_width,
-    int $src_height
+    GdImage $dst_image, -> Imagem destino
+    GdImage $src_image, -> Imagem de origem
+    int $dst_x, -> Onde o eixo x deve começar na imagem destino
+    int $dst_y, -> Onde o eixo Y deve começar na imagem de origem
+    int $src_x, -> Onde o eixo x deve começar na imagem de origem
+    int $src_y, -> Onde o eixo Y deve começar na imagem de origem
+    int $dst_width, -> Largura da imagem destino
+    int $dst_height, -> Altura da imagem destino
+    int $src_width, -> Largura da imagem de origem
+    int $src_height -> Altura da imagem de origem
 ): bool
 */
-// imagecopyresampled($newImage, $oldImage, );
+
+// Dimensionando a nova imagem a partir da imagem de origem
+imagecopyresampled($newImage, $oldImage, 0, 0, 0, 0, $new_width, $new_height, $oldWidth, $oldHeight);
+
+// Gerando a imagem destino em jpg
+imagejpeg($newImage);
+
+// Destruindo as duas imagens da memória
+imagedestroy($oldImage);
+imagedestroy($newImage);
 
 ?>
