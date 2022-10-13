46e3a8786a8cf36a3ba529f2310606e6187d3feb
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-10-13 11:55:18 -0300

    :rocket: learning about composer

---
+++ 17-gerenciando-dependencias-com-composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'joaolucas2012/composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ba2b1c8dfce631231ea3928523c601bad093e867',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'joaolucas2012/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ba2b1c8dfce631231ea3928523c601bad093e867',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
